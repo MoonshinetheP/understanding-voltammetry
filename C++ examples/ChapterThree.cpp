a2eec4dfe78a3e3093a429fd787ffd9b80f6251e
Steven Linfield <S.Linfield@outlook.com>
2023-02-05 19:27:51 +0100

    Adding files

---
+++ C++ examples/ChapterThree.cpp
@@ -0,0 +1,58 @@
+#include <fstream> // Header for file output
+#include <vector> // Header for vectors
+#include <cmath> // Header for sqrt(), fabs()
+int main()
+{
+// Specify simulation parameters
+double theta_i = 20.0;
+double theta_v = -20.0;
+double sigma = 100.0;
+double deltaX = 2e-4;
+double deltaTheta = 0.02;
+//Calculate other parameters
+double deltaT = deltaTheta / sigma;
+double maxT = 2.0 * fabs(theta_v - theta_i) / sigma;
+double maxX = 6.0*sqrt(maxT);
+int n = (int)( 1.0 + maxX / deltaX ); // number of spacesteps
+int m = (int)( maxT / deltaT ); // number of timesteps
+// Calculate Thomas coefficients
+double lambda = deltaT / (deltaX*deltaX);
+double alpha = -lambda;
+double beta = 2.0*lambda + 1.0;
+double gamma = -lambda;
+// Create containers
+std::vector<double> g_mod(n-2, 0.0);
+std::vector<double> delta(n-1, 1.0);
+std::vector<double> d_mod(n-1, 0.0);
+std::vector<double> C(n, 1.0); // concentration profile
+// Modify gamma coefficients
+g_mod[0] = 0; // boundary condition
+for(int i=1; i<n-2; i++) {
+g_mod[i] = gamma / (beta - g_mod[i-1] * alpha);
+}
+// Open file to output CV
+std::ofstream CV("CV_Output.txt");
+// BEGIN SIMULATION
+double Theta = theta_i;
+for(int k=0; k<m; k++)
+{
+if( k < m / 2 ) { Theta -= deltaTheta; }
+else { Theta += deltaTheta; }
+// Forward sweep - create modified deltas
+d_mod[0] = 1.0 / (1.0 + exp(-Theta));
+for(int i=1; i<n-1; i++) {
+d_mod[i] = ( delta[i] - d_mod[i-1]*alpha )
+/ ( beta - g_mod[i-1] * alpha );
+}
+// Back Substitution
+C[n-1] = 1.0;
+for(int i=n-2; i>=0; i--) {
+C[i] = d_mod[i] - g_mod[i] * C[i+1];
+delta[i] = C[i];
+}
+//Output current
+double flux = -(-C[2] + 4.0*C[1] -3.0*C[0]) / (2.0*deltaX);
+CV << Theta << "\t" << flux << "\n";
+}
+// END SIMULATION
+}
---
+++ Python examples/ChapterFour.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+class ChFour:
+
+    def __init__(self, theta = -15, tmax = 1, omegax = 1.05, h = 1E-4, omegat = 1.015):
+        
+        self.theta = theta
+        self.tmax = tmax
+        self.omegax = omegax
+        self.h = h
+        self.omegat = omegat
+        
+        self.deltat = self.tmax / 4000
+        self.ts = self.tmax / 2
+
+        self.maxx = 6 * np.sqrt(self.tmax)
+        
+        '''Expanding grid'''
+        self.x = np.array([0])
+        while self.x[-1] < self.maxx:
+            self.x = np.append(self.x, self.x[-1] + self.h)
+            self.h *= self.omegax
+
+        self.n = int(self.x.size)
+        
+        '''Expanding time'''
+        self.t = np.array([0])
+        while self.t[-1] < self.tmax:
+            self.t = np.append(self.t, self.t[-1] + self.deltat)
+            if self.t[-1] > self.ts:
+                self.deltat *= self.omegat
+
+        self.m = int(self.t.size)
+        
+        '''Containing arrays'''
+        self.alpha = np.zeros((self.n - 1,))
+        self.beta = np.zeros((self.n - 1,))
+        self.gamma = np.zeros((self.n - 1,))
+        self.gmod = np.zeros((self.n - 1,))
+        self.delta = np.ones((self.n - 1,))
+        self.dmod = np.zeros((self.n - 1,))
+        self.C = np.ones((self.n,))
+
+        
+        '''Thomas coefficients for T < Ts'''
+        self.delt = self.t[-1] - self.t[0]
+        self.delx = np.zeros((self.n,))
+        self.delx[0] = self.x[1] - self.x[0]
+
+        for i in range(1, self.n - 1):
+            self.delx[i] = self.x[i + 1] - self.x[i]
+
+            self.alpha[i] = -(2 * self.delt) / (self.delx[i - 1] * (self.delx[i - 1] + self.delx[i]))
+            self.gamma[i] = -(2 * self.delt) / (self.delx[i] * (self.delx[i - 1] + self.delx[i]))
+            self.beta[i] = 1 - self.alpha[i] - self.gamma[i]
+        
+
+        '''Modified gamma coefficients for T < Ts'''   
+        self.gmod[0] = 0
+        for i in range(1, self.n -1, 1):
+            self.gmod[i] = self.gamma[i] / (self.beta[i] - self.gmod[i-1] * self.alpha[i]) 
+
+
+    def run(self):
+
+        self.time = np.array([])
+        self.flux = np.array([])
+        self.qj = np.array([])
+
+        for k in range(1, self.m, 1):
+            if self.t[k] > self.ts:
+                for i in range(1, self.n - 1):
+                    self.delt = self.t[k] - self.t[k-1]
+                    self.alpha[i] = -(2 * self.delt) / (self.delx[i - 1] * (self.delx[i - 1] + self.delx[i]))
+                    self.gamma[i] = -(2 * self.delt) / (self.delx[i] * (self.delx[i - 1] + self.delx[i]))
+                    self.beta[i] = 1 - self.alpha[i] - self.gamma[i]
+            
+                self.gmod[0] = 0
+                for i in range(1, self.n - 1):
+                    self.gmod[i] = self.gamma[i] / (self.beta[i] - self.gmod[i - 1] * self.alpha[i])
+                
+            self.dmod[0] = 1 / (1 + np.exp(-self.theta))
+            for i in range(1, self.n - 1):
+                self.dmod[i] = (self.delta[i] - self.dmod[i - 1] * self.alpha[i]) / (self.beta[i] - self.gmod[i - 1] * self.alpha[i])
+
+            self.C[self.n - 1] = 1
+            for i in range(self.n - 2, -1, -1):
+                self.C[i] = self.dmod[i] - self.gmod[i] * self.C[i + 1]
+
+                self.delta[i] = self.C[i]
+
+            self.time = np.append(self.time, self.t[k])
+            self.flux = np.append(self.flux, -(self.C[1] - self.C[0]) / (self.x[1] - self.x[0]))
+            self.ref = -1 / np.sqrt(np.pi * self.t[k])
+            self.qj = np.append(self.qj, (self.flux[-1] - self.ref) / (self.ref * 100))
+
+        self.dist = np.array([])
+        self.conc = np.array([])    
+        for i in range(0, self.n):
+            self.dist = np.append(self.dist, self.x[i])
+            self.conc = np.append(self.conc, self.C[i])
+        
+        self.output = zip(self.time, self.flux, self.qj)
+        self.output2 = zip(self.dist, self.conc)
+        return(self.output2)
